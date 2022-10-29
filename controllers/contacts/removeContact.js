4fe4472e7cc8707ee524c753ce9ada881db72cc3
MaximeWishnevski <velolivefan@gmail.com>
2022-10-29 17:12:14 +0300

    changes

--- controllers/contacts/removeContact.js
+++ controllers/contacts/removeContact.js
@@ -1,13 +1,15 @@
-const contacts = require("../../service/contacts");
+const { Contact } = require("../../models/contact");
 const { createError } = require("../../helpers/createError");
 
 const removeContact = async (req, res, next) => {
   const { contactId } = req.params;
-  const result = await contacts.removeContact(contactId);
+  const result = await Contact.findByIdAndRemove(contactId);
   if (!result) {
     throw createError(404);
   }
   res.json({ message: "contact deleted" });
 };
+
+module.exports = removeContact;
 
 module.exports = removeContact;
