af19a23ea79b7da7c84fda44609ea8fe793f3992
MaximeWishnevski <velolivefan@gmail.com>
2022-11-03 09:25:56 +0200

    mail

--- controllers/auth/auth.test.js
+++ controllers/auth/auth.test.js
@@ -1,5 +1,4 @@
 const request = require("supertest");
-
 const app = require("../../app");
 const { User } = require("../../models/user");
 const mongoose = require("mongoose");
--- server.js
+++ server.js
@@ -11,5 +11,5 @@ mongoose
   .catch((error) => {
     console.log(error.message);
     process.exit(1);
-});
+  });
   
